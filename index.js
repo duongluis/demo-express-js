f08dbfe4cae48148a906669e3ddbc4f8083475ef
duongluis <nthaiduong9920@gmail.com>
2023-01-09 18:10:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,35 @@ expressApp.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+bot.command('start', ctx => {
+  console.log(ctx.from)
+  bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it', {
+  })
+})
+
+bot.command('ethereum', ctx => {
+  var rate;
+  console.log(ctx.from)
+  axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`)
+  .then(response => {
+    console.log(response.data)
+    rate = response.data.ethereum
+    const message = `Hello, today the ethereum price is ${rate.usd}USD`
+    bot.telegram.sendMessage(ctx.chat.id, message, {
+    })
+  })
+})
+
+bot.command('https...', ctx => {
+  console.log(ctx.from)
+  var id = JSON.parse(link)
+  var array = id.split('/')
+  var max = array.length 
+  var message = `${array[max - 1]}`
+  bot.telegram.sendMessage(ctx.chat.id, message, {
+  })
+})
+
+
+
 bot.launch()
